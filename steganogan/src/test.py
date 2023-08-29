184deb5344fb4326da15b033619b57c78583a95f
sonnh-uit <sonnh.uit@gmail.com>
2023-08-29 14:53:55 +0700

    update project to convert channel

--- steganogan/src/test.py
+++
@@ -1,9 +0,0 @@
-from steganogan import SteganoGAN
-steganogan = SteganoGAN.load(architecture='dense')
-
-# steganogan.encode('input/lena.png', 'results/output.png', 'This is a super secret message!')
-
-# print(steganogan.decode('results/output.png'))
-steganogan.decode('results/output.png')
-
-# sudo docker compose run steganogan steganogan encode lena.png 'this is secret message'
